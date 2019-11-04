3dca6fe79ce3d0e2fe081e9ae2f70e3b2e6d156e
deosc <oz_colors@hotmail.com>
2019-11-03 21:00:47 -0600

    Se cambia diff

--- pyhton/practicas/ws/diffImp.py
+++ pyhton/practicas/ws/diffImp.py
@@ -5,7 +5,7 @@ import consoleExecute as consoleExecute
 def main_implementation(ipSource, ipTarget):
     print(ipSource)
     print (ipTarget)
-    comand = "wdiff /tmp/{0}.backup /tmp/{1}.backup"
+    comand = "wdiff -3 -wd -xd -yi -zi /tmp/{0}.backup /tmp/{1}.backup"
     return consoleExecute.run_command(comand.format(ipSource,ipTarget))
 
 
