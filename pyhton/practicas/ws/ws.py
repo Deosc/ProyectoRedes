22a5e625898d1c14baf497f5bf1c6ae402635339
charlis397 <charlis397@hotmail.com>
2019-11-29 13:37:45 -0600

    Se actualizan servicios

--- pyhton/practicas/ws/ws.py
+++ pyhton/practicas/ws/ws.py
@@ -1,5 +1,6 @@
 import json
 import hug
+import pymongo
 import requests
 import netflowImp as netflow
 import netflowImp2 as netflowStatics
@@ -8,6 +9,13 @@ import diffImp as diff
 import host_discoverer as hd
 import uploadConfig as uploadConfig
 import changes as changesImp
+import telnetlib
+from os import remove
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["proyect"]
+mycol = mydb["routers"]
+templates = mydb["templates"]
 
 
 
@@ -60,3 +68,117 @@ def netflowScan(ipSource, ipTarget):
         "diff": stringReturn
     }
     return x
+
+@hug.get(examples='archivo_conf=10.0.27.1.txt')
+@hug.local()
+
+def template(archivo_conf: hug.types.text):
+
+	ENCONTRADO = 0
+
+	interfaces = []
+	ip_interface = []
+	ip_routes = []
+
+	#Se crea la plantilla del router
+	f = open(archivo_conf, "r")
+	for linea in f:
+
+		if linea.find("hostname") == ENCONTRADO:
+			router = linea.strip()
+			print(linea.strip())
+
+		if linea.find("interface") == ENCONTRADO:
+			interfaces.append([{"interface":linea.strip()},{"ip address":""}])
+			aux = linea.strip()
+		if linea.lstrip().find("ip address") == ENCONTRADO:
+			interface = interfaces.pop()
+			interface=[{"interface":aux},{"ip address":linea.strip()}]
+			interfaces.append(interface)
+		if linea.lstrip().find("shutdown") == ENCONTRADO:
+			interface = interfaces.pop()
+			interface=[{"interface":aux},{"ip address":linea.strip()}, {"shutdown":linea.strip()}]
+			interfaces.append(interface)
+
+		if linea.lstrip().find("ip route") == ENCONTRADO:
+			ip_route = [{"ip route":linea.strip()}]
+			ip_routes.append(ip_route)
+
+
+		#or linea.lstrip().find("ip route") == ENCONTRADO 	if
+	print(interfaces)
+	document = { "router": router, "interfaces": interfaces, "ip routes": ip_routes}
+	#document = { "prueba": prueba}
+	mycol.insert_one(document)
+
+	f.close()
+
+@hug.get("/insert")
+@hug.local()
+def insertTemplate():
+	template = mycol.find_one()
+	template_found = templates.find_one({},{ "router": template["router"]})
+	if template_found is None:
+		templates.insert_one(template)
+	else:
+		myquery = { "router": template["router"]}
+		newvalues = { "$set":  template }
+		templates.update_one(myquery, newvalues)
+
+	mycol.delete_many({})
+
+@hug.get("/comparate")
+@hug.local()
+def comparate(routerIn: hug.types.text):
+	router = mycol.find_one({},{"_id":0})
+	template_found = templates.find_one({ "router": routerIn},{"_id":0})
+	mycol.delete_many({})
+	return {router == template_found}
+
+@hug.get("/telnet")
+@hug.local()
+def telnet(host: hug.types.text):
+	ENCONTRADO = 0
+	user = "usuario"
+	password = "password"
+
+	#Abrimos archivo donde colocaremos la informacion
+	archivo = open(host+"Aux.txt", "w+")
+
+	#Conexion Telnet
+	tn = telnetlib.Telnet(host)
+
+	#Conexion al router
+	tn.read_until(b"Username: ")
+	tn.write(user.encode('ascii') + b"\n")
+	if password:
+	    tn.read_until(b"Password: ")
+	    tn.write(password.encode('ascii') + b"\n")
+
+	#Colocamos los comandos
+	tn.write(b"terminal length 0\n")
+	tn.write(b"show version\n")
+	tn.write(b"end\n")
+	tn.write(b"exit\n")
+
+	#Escribimos y cerramos el archivo
+	archivo.write (tn.read_all().decode('ascii'))
+	archivo.close()
+
+
+	f = open(host+"Aux.txt", "r")
+	newFile = open(host, "w")
+	for linea in f:
+
+		if linea.find("Cisco IOS") == ENCONTRADO:
+			newFile.write(linea.strip())
+			print(linea.strip())
+		if linea.find("PCI") == ENCONTRADO:
+			newFile.write(linea.strip())
+			print(linea.strip())
+		if linea.strip().find("FastEthernet") == ENCONTRADO:
+			newFile.write(linea.strip())
+			print(linea.strip())
+
+	remove(host+"Aux.txt")
+	return {'message': 'Conexion al router {0} correcto!'.format(host)}
